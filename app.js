e547c5930aa80b4f6bd60d24b4c021decca2d6da
mk <mk@mks-imac.lan>
2019-09-29 16:59:51 -0400

    Updates: initial commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var peopleRouter = require('./routes/people');
+var codelabRouter = require('./routes/codelab');
 
 var app = express();
 
@@ -20,7 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/people', peopleRouter);
+app.use('/codelab', codelabRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
