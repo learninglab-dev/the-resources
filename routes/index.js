1553df67dd816e2974501b9e954304ee367641c8
mk <mk@mks-MacBook-Air.local>
2019-10-10 09:00:27 -0400

    Updates=

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ var frontPageLinks = require('../data/front-page-links.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "show and tell", message: `all the things as of ${moment().format('YYYYMMDD')}`, links: frontPageLinks})
+  res.render('index', { title: "the show", message: `all the things as of ${moment().format('YYYYMMDD')}`, links: frontPageLinks})
+});
+
+router.get('/and-tell', function(req, res, next) {
+  res.render('index', { title: "show (and tell)", message: `all the things as of ${moment().format('YYYYMMDD')}`, links: frontPageLinks})
 });
 
 module.exports = router;
