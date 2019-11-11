3f0c15670729fd529ef937d942a382d66698f9bf
mk <mk@mks-MacBook-Pro-3.local>
2019-11-11 09:21:15 -0500

    Updates:

--- routes/work.js
+++ routes/work.js
@@ -4,6 +4,8 @@ const marked = require('marked');
 const fs = require('fs');
 const path = require('path');
 var moment = require('moment');
+var recursive = require("recursive-readdir");
+
 
 router.get('/', function(req, res, next) {
   var markdownFiles = fs.readdirSync(path.join(ROOT_DIR, 'data/work'));
@@ -29,69 +31,85 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', async function (req, res, next) {
-  var markdownFiles = fs.readdirSync(path.join(ROOT_DIR, 'data/work'));
-  if (markdownFiles.includes(`${req.params.id}.md`)) {
-    fs.readFile(path.join(ROOT_DIR, 'data/work', `${req.params.id}.md`), {encoding: 'utf-8'}, (err, data) => {
-      if (err) {res.send("check back in a minute")};
-      console.log(data);
-      if (/!\[.*\]\(.*\)/.test(data)) {
-        console.log("there's an image in the .md file!");
-        var heroImage = data.match(/!\[.*\]\(.*\)/)
-          .toString()
-          .replace(/!\[.*\]\(/, "")
-          .replace(")", "");
+  console.log(`checking ${ROOT_DIR}/data/work/${req.params.id}`);
+  if (fs.existsSync(`${ROOT_DIR}/data/work/${req.params.id}.md`)) {
+    var markdownPath = `${ROOT_DIR}/data/work/${req.params.id}.md`;
+    try {
+      if (fs.existsSync(markdownPath)) {
+        fs.readFile(markdownPath, {encoding: 'utf-8'}, (err, data) => {
+          if (err) {res.send("check back in a minute")};
+          console.log(data);
+          if (/!\[.*\]\(.*\)/.test(data)) {
+            console.log("there's an image in the .md file!");
+            var heroImage = data.match(/!\[.*\]\(.*\)/)
+              .toString()
+              .replace(/!\[.*\]\(/, "")
+              .replace(")", "");
+          } else {
+            console.log("no hero image, let's go with crayons");
+            var heroImage = "https://live.staticflickr.com/2871/33129125296_1ef184d0c9_h.jpg"
+          }
+          res.render("work",  {
+            title: `${path.basename(markdownPath, '.md')}`,
+            convertedMarkdown: marked(data),
+            heroImage: heroImage
+          });
+        });
       } else {
-        console.log("no hero image, let's go with crayons");
-        var heroImage = "https://live.staticflickr.com/2871/33129125296_1ef184d0c9_h.jpg"
+        res.render('work', {title: "no file found", heroImage: "https://live.staticflickr.com/2871/33129125296_1ef184d0c9_h.jpg", convertedMarkdown: `didn't find anything here: ${markdownPath}`})
       }
-      res.render("work",  {
-        title: `${req.params.id}`,
-        convertedMarkdown: marked(data),
-        heroImage: heroImage
+    } catch(err) {
+      console.error(err)
+    }
+  } else {
+    recursive(`${ROOT_DIR}/data/work/${req.params.id}`, function (err, files) {
+      var links = [];
+      if (files && files.length) {
+        for (var i = 0; i < files.length; i++) {
+          links.push({
+            url: `/work/${req.params.id}/${(files[i].split(`/work/${req.params.id}`)[1]).split('.md')[0 ]}`,
+            linkTitle: `${files[i].split("/work/")[1]}`
+          })
+        }
+      }
+      res.render('index', {
+        date: moment(),
+        message: `all the links to work for ${req.params.id}`,
+        title: `${req.params.id} work`,
+        links: links
       });
     });
-  } else if (fs.existsSync(path.join(ROOT_DIR, 'data/work', req.params.id)) && fs.lstatSync(path.join(ROOT_DIR, 'data/work', req.params.id)).isDirectory()) {
-    var markdownFiles = fs.readdirSync(path.join(ROOT_DIR, 'data/work', req.params.id));
-    var linksList = [];
-    for (var i = 0; i < markdownFiles.length; i++) {
-      if (/\.md/g.test(markdownFiles[i])) {
-        linksList.push({
-          url: `/work/${req.params.id}/${path.basename(markdownFiles[i], '.md')}`,
-          linkTitle: `${path.basename(markdownFiles[i], '.md').replace(/-/g, " ")}`,
-          text: ""
-      })
-      }
-    }
-    res.render("index", { title: 'show (your work)', date: moment(), message: `all the links to work for ${req.params.id}`,links: linksList })
-  } else {
-    res.render('test', {title: "nothing here just yet.", data: JSON.stringify(markdownFiles, null, 4)});
   }
 });
 
-router.get('/:folderId/:fileId', async function (req, res, next) {
-  var markdownFiles = fs.readdirSync(path.join(ROOT_DIR, 'data/work', req.params.folderId));
-  if (markdownFiles.includes(`${req.params.fileId}.md`)) {
-    fs.readFile(path.join(ROOT_DIR, 'data/work', req.params.folderId, `${req.params.fileId}.md`), {encoding: 'utf-8'}, (err, data) => {
-      if (err) {res.send("check back in a minute")};
-      console.log(data);
-      if (/!\[.*\]\(.*\)/.test(data)) {
-        console.log("there's an image in the .md file");
-        var heroImageMd = data.match(/!\[.*\]\(.*\)/);
-
-        var heroImage = heroImageMd.toString().replace(/!\[.*\]\(/, "").replace(")", "");
-        console.log(`the heroImage is ${heroImage}`);
-      } else {
-        console.log("no hero image, let's go with crayons");
-        var heroImage = "https://live.staticflickr.com/2871/33129125296_1ef184d0c9_h.jpg"
-      }
-      res.render("work",  {
-        title: `${req.params.fileId}`,
-        convertedMarkdown: marked(data),
-        heroImage: heroImage
+router.get('/:folderId/*', async function (req, res, next) {
+  var markdownPath = `${path.join(ROOT_DIR, 'data/work', req.params.folderId, req.params[0])}.md`;
+  try {
+    if (fs.existsSync(markdownPath)) {
+      fs.readFile(markdownPath, {encoding: 'utf-8'}, (err, data) => {
+        if (err) {res.send("check back in a minute")};
+        console.log(data);
+        if (/!\[.*\]\(.*\)/.test(data)) {
+          console.log("there's an image in the .md file!");
+          var heroImage = data.match(/!\[.*\]\(.*\)/)
+            .toString()
+            .replace(/!\[.*\]\(/, "")
+            .replace(")", "");
+        } else {
+          console.log("no hero image, let's go with crayons");
+          var heroImage = "https://live.staticflickr.com/2871/33129125296_1ef184d0c9_h.jpg"
+        }
+        res.render("work",  {
+          title: `${path.basename(markdownPath, '.md')}`,
+          convertedMarkdown: marked(data),
+          heroImage: heroImage
+        });
       });
-    });
-  } else {
-    res.render('test', {title: "nothing here just yet.", data: JSON.stringify(markdownFiles, null, 4)});
+    } else {
+      res.render('work', {title: "no file found", heroImage: "https://live.staticflickr.com/2871/33129125296_1ef184d0c9_h.jpg", convertedMarkdown: `didn't find anything here: ${markdownPath}`})
+    }
+  } catch(err) {
+    console.error(err)
   }
 });
 
