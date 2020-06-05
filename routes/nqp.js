6fd14997b2a13665734684d13e6ea8115ef16234
mk <mk@mac-mini-2.lan>
2020-06-05 12:41:15 -0400

    Updates:

--- routes/nqp.js
+++ routes/nqp.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const marked = require('marked');
 var prepMarkdown = require("../tools/utilities/prep-markdown");
 var Airtable = require("airtable");
+var fm = require('front-matter');
 
 router.get('/:id', async function(req, res, next) {
   console.log("request for nqp id " + req.params.id);
@@ -12,15 +13,15 @@ router.get('/:id', async function(req, res, next) {
   base("NotQuitePinterestBut")
     .find(req.params.id)
     .then(result=>{
+      var theStuff = result.fields.completedMarkdown ? fm(result.fields.completedMarkdown) : fm(result.fields.draftMarkdown);
       res.render("resource", {
-        title: result.fields.title ? result.fields.title
-          : result.fields.title ? result.fields.title
+        title: theStuff.attributes.title ? theStuff.attributes.title
           : req.params.id,
-        thumbnail: result.fields.ogImage ? result.fields.ogImage : "https://www.84lumber.com/media/1333/lumber_cover.jpg",
-        description: result.fields.description ? result.fields.description : "A resource from the Learning Lab",
-        convertedMarkdown: result.fields.completedMarkdown ? marked(result.fields.completedMarkdown)
-          : result.fields.draftMarkdown ? marked(result.fields.draftMarkdown)
-          : ""
+        thumbnail: theStuff.attributes.thumbnail ? theStuff.attributes.thumbnail
+          : result.fields.ogImage ? result.fields.ogImage : "https://www.84lumber.com/media/1333/lumber_cover.jpg",
+        description: theStuff.attributes.description ? theStuff.attributes.description
+          : result.fields.description ? result.fields.description : "A resource from the Learning Lab",
+        convertedMarkdown: marked(theStuff.body)
       });
     })
     .catch((err)=>{res.send(`couldn't find ${req.params.id}`)})
